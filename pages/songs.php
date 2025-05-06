f5f0bdfdbe9bac2ef27b961a4cd8c3b702b09f64
Josh White <whitejosh429@gmail.com>
2025-05-06 15:32:55 +0100

    path update

--- pages/songs.php
+++ pages/songs.php
@@ -45,10 +45,8 @@
                     <select name="discog">
                         <option value="">-- Any --</option>
                         <option value="main" <?php if (($_GET['discog'] ?? '') === 'main') echo 'selected'; ?>>Main</option>
-                        <option value="beatMARIO" <?php if (($_GET['discog'] ?? '') === 'beatMARIO') echo 'selected'; ?>>Pre-2010</option>
-                        <option value="Remix" <?php if (($_GET['discog'] ?? '') === 'Remix') echo 'selected'; ?>>Remix</option>
-                        <option value="Single" <?php if (($_GET['discog'] ?? '') === 'Single') echo 'selected'; ?>>Single</option>
-                        <option value="Cover" <?php if (($_GET['discog'] ?? '') === 'Cover') echo 'selected'; ?>>Cover</option>
+                        <option value="pre-2010" <?php if (($_GET['discog'] ?? '') === 'pre-2010') echo 'selected'; ?>>Pre-2010</option>
+                        <option value="other" <?php if (($_GET['discog'] ?? '') === 'other') echo 'selected'; ?>>Other</option>
                     </select>
                 </div>
             </label>
